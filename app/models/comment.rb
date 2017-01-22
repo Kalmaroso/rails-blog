504e0ddfb504b5a74f7047dd3b9bc54fe53bfc62
Matthew Weingarten <matt@weingarten.org>
2017-01-21 22:27:07 -0800

    Adds assosiations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  has_many :votes, as: :votable
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :votes, as: :votable
+  has_many :comments
 end
