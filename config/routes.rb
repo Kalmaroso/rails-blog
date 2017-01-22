122b343bfd5dfedb1926b4233fb29e35acaa0ef4
Matthew Weingarten <matt@weingarten.org>
2017-01-22 15:44:34 -0800

    Adds controllers, routes and view folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "posts#index"
+  
+  resources :posts
+
+  get "/comments/new", to: "comments#new"
+  post "/comments", to: "comments#create"
+  get "comments/:id/edit", to: "comments#edit"
+  put "comments/:id", to: "comments#update"
+  delete "comments/:id", to: "comments#destroy"
+
+  post "/votes", to: "votes#create"
+  put "votes/:id", to: "votes#update"
+  delete "votes/:id", to: "votes#destroy"
+
+  get "/users/new", to: "users#new"
+  get "/:username", to: "users#show", as: :user
+  post "/users", to: "user#create"
+
+  get "/login", to: "sessions#new"
+  post "/sessions/new", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
